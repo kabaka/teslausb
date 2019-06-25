dcdca53da33bf59c8a4d0ec008b1cfef97c605cc
Kyle Johnson <kyle@vacantminded.com>
2019-06-25 04:40:29 +0000

    r/rclone/a-c: add tesla_api support, dirs, rm
    For rclone desintations, add:
    
    - Tesla API support. After each file, attempt the wake up the vehicle.
      This is not the same as the CIFS support for Tesla API, which makes an
      API call only if three minutes has passed. That would be trivial to
      add, but I haven't done so yet.
    - Upload directories, not just the video files within them. As
      implemented, rclone uploaded all videos to the target directory,
      discarding the containing directory. This made it basically impossible
      to locate discreet groups of videos. Instead, first create the target
      directory, then upload the files to it.
    - The rclone command "moves" files – the source files _are_ deleted.
      However, the source directories remain, cluttering the USB drive. I do
      not believe there is an rclone invokation which will upload and delete
      the directories, so `rm` everything in SavedClips after upload is
      finished.

--- run/rclone_archive/archive-clips.sh
+++ run/rclone_archive/archive-clips.sh
@@ -6,14 +6,33 @@ source /root/.teslaCamRcloneConfig
 
 NUM_FILES_MOVED=0
 
+function keep_car_awake() {
+  # If the tesla_api.py script is installed, send the car a wake_up command.
+  if [ -f /root/bin/tesla_api.py ]
+  then
+    /root/bin/tesla_api.py wake_up_vehicle >> "$LOG_FILE"
+  fi
+}
+
 for file_name in "$CAM_MOUNT"/TeslaCam/saved* "$CAM_MOUNT"/TeslaCam/SavedClips/*; do
   [ -e "$file_name" ] || continue
-  log "Moving $file_name ..."
-  rclone --config /root/.config/rclone/rclone.conf move "$file_name" "$drive:$path" >> "$LOG_FILE" 2>&1 || echo ""
-  log "Moved $file_name."
+  DIR_NAME="$(basename $file_name)"
+  log "Creating destination directory $file_name"
+  rclone --config /root/.config/rclone/rclone.conf mkdir "$drive:$path/$DIR_NAME/" >> "$LOG_FILE" 2>&1 || echo ""
+  log "Moving contents of $file_name"
+  rclone --config /root/.config/rclone/rclone.conf move "$file_name" "$drive:$path/$DIR_NAME/" >> "$LOG_FILE" 2>&1 || echo ""
+  log "Moved $file_name"
   NUM_FILES_MOVED=$((NUM_FILES_MOVED + 1))
+
+  # XXX
+  keep_car_awake || true
 done
-log "Moved $NUM_FILES_MOVED file(s)."
+
+log "Moved files for $NUM_FILES_MOVED directories."
+
+rmdir --ignore-fail-on-non-empty "$CAM_MOUNT/TeslaCam/SavedClips"/* || true
+
+log "Erased remaining SavedClips contents."
 
 /root/bin/send-push-message "$NUM_FILES_MOVED"
 
